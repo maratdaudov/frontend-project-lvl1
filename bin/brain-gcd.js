832fba34d6375eb08743791345cc85e1aca0e6d4
marat <maratdaudov93@gmail.com>
2021-08-12 22:36:29 +0600

    Init brain-jcd game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import gcd from '../games/gcd.js';
+import runGame from '../src/index.js';
+
+runGame(gcd);
---
+++ games/gcd.js
@@ -0,0 +1,10 @@
+import { getRandomNumber } from '../src/index.js';
+
+const getQuestionAndAnswerCorrect = () => {
+  console.log('Find the greatest common divisor of given numbers.');
+
+  const number1 = getRandomNumber();
+  const number2 = getRandomNumber();
+};
+
+export default getQuestionAndAnswerCorrect;
