e0bd76bc06b99caecc74cb8495c84821a76dc68b
marat <maratdaudov93@gmail.com>
2021-08-25 01:35:34 +0600

    Add functional for progression game

--- games/progression.js
+++ games/progression.js
@@ -1,5 +1,29 @@
+import { getRandomNumber } from '../src/index.js';
+
 const getQuestionAndAnswerCorrect = () => {
-  console.log('Welcome to Brain Games!');
+  console.log('What number is missing in the progression?');
+
+  const randomNumber = getRandomNumber(0, 100);
+  const progressionLength = getRandomNumber(5, 10);
+  const step = getRandomNumber(1, 5);
+  const hiddenPosition = getRandomNumber(1, 5);
+
+  let question = '';
+  let answerCorrect = null;
+  const arr = [randomNumber];
+
+  for (let i = 0; i < progressionLength; i += 1) {
+    arr[i + 1] = arr[i] + step;
+
+    if (i === hiddenPosition) {
+      arr[i] = '..';
+      answerCorrect = arr[i - 1] + step;
+    }
+  }
+
+  question = arr.join(' ');
+
+  return [question, answerCorrect];
 };
 
 export default getQuestionAndAnswerCorrect;
