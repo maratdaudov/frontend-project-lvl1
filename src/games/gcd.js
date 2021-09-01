ad401fc2ce4642c1b810e9eb6bf8b0ec4189e8b9
marat <maratdaudov93@gmail.com>
2021-09-02 01:13:46 +0600

    Refactor gcd game

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,22 +2,20 @@ import { getRandomNumber } from '../index.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
+const gcd = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+
+  return gcd(b, a % b);
+};
+
 const getQuestionAndAnswerCorrect = () => {
   const number1 = getRandomNumber();
   const number2 = getRandomNumber();
   const question = `${number1} ${number2}`;
 
-  const minNumber = Math.min(number1, number2);
-  let answerCorrect = null;
-
-  for (let i = minNumber; i >= 1; i -= 1) {
-    if ((number1 % i === 0 && number2 % i === 0) || (number1 === number2)) {
-      answerCorrect = i;
-      break;
-    }
-
-    answerCorrect = 1;
-  }
+  const answerCorrect = gcd(number1, number2);
 
   return [question, answerCorrect];
 };
