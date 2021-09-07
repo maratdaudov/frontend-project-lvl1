0b6118c614fa429fa0f6427ead7cfc456891b4b5
marat <maratdaudov93@gmail.com>
2021-09-07 23:18:12 +0600

    Refactor progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,24 +3,24 @@ import runGame from '../index.js';
 
 const description = 'What number is missing in the progression?';
 
-const lengthProgression = getRandomNumber(5, 10);
-
-const makeProgression = (startNumber, step) => {
+const makeProgression = (startNumber, step, length) => {
   const progression = [];
 
-  for (let i = 0; i <= lengthProgression; i += 1) {
-    progression[i] = startNumber + (step * i);
+  for (let i = 0; i <= length; i += 1) {
+    progression.push(startNumber + (step * i));
   }
 
   return progression;
 };
 
 const getGameData = () => {
+  const lengthProgression = 10;
   const startNumber = getRandomNumber(0, 100);
   const step = getRandomNumber(1, 5);
-  const progression = makeProgression(startNumber, step);
+  const progression = makeProgression(startNumber, step, lengthProgression);
   const hiddenNumber = getRandomNumber(0, lengthProgression);
   const correctAnswer = progression[hiddenNumber];
+
   progression[hiddenNumber] = '..';
   const question = progression.join(' ');
 
