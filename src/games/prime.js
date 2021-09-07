99532a4964232b30f1de6dcd802dc95647ac67a9
marat <maratdaudov93@gmail.com>
2021-09-07 22:16:45 +0600

    Refactor inner function in prime.js file

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,23 +4,22 @@ import runGame from '../index.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no"';
 
 const isPrime = (num) => {
-  let result = 'yes';
   const sqrt = Math.sqrt(num);
 
   for (let i = 2; i <= sqrt; i += 1) {
     if (num % i === 0) {
-      result = 'no';
+      return false;
     }
   }
 
-  return result;
+  return true;
 };
 
 const getGameData = () => {
   const randomNumber = getRandomNumber(0, 100);
 
   const question = randomNumber;
-  const correctAnswer = isPrime(randomNumber);
+  const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
 
   return [question, correctAnswer];
 };
