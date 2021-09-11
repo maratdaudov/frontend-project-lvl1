6acd0a46ee702f04391e37de7f359643010a93e0
marat <maratdaudov93@gmail.com>
2021-09-11 15:45:05 +0600

    Add guard expression in prime game

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,6 +6,10 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 const isPrime = (num) => {
   const sqrt = Math.sqrt(num);
 
+  if (num < 2) {
+    return false;
+  }
+
   for (let i = 2; i <= sqrt; i += 1) {
     if (num % i === 0) {
       return false;
